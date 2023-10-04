0e7c37561c4b576c7d0c6d258ac4ef6023b285cf
Apurva167 <146975358+Apurva167@users.noreply.github.com>
2023-10-04 22:44:49 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,12 +16,25 @@ public class Main {
 	        homeSystem.turnOn(1);
 	        homeSystem.scheduleDevice(2,"06:00","Turn On");
 	        homeSystem.addAutomatedTrigger("thermostat",">", 75, "turnOff(1");
-	        System.out.println("1.Status Report\n2.ScheduledTasks \n3.Automated Triggers");
-	        int option=scanner.nextInt();
-	        switch(option) {
-	        case 1:
-	        	homeSystem.statusReport();
-	        	break;
+	       
+	        outerLoop:
+	        while(true) {
+	        	System.out.println("1.Status Report\n2.ScheduledTasks \n3.Automated Triggers \n0.exit");
+	 	        int option=scanner.nextInt();
+	            switch(option) {
+	             case 1:
+	        	    homeSystem.statusReport();
+	        	    break;
+	             case 2:
+	        	    homeSystem.scheduledTask();
+	        	    break;
+	             case 3:
+	             	homeSystem.automatedTriggers();
+	        	    break;
+	             case 0:
+	                break outerLoop;
+	            }
+	            System.out.println("\n");
 	        }
 	}
 
